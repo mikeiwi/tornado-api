0b2d0ce04d7b03111a71ed02f9cda6519d5927d3
Mike <michael.valenciav@gmail.com>
2019-02-07 21:57:26 -0500

    Loan endpoint added to home.

--- home.py
+++ home.py
@@ -1,13 +1,24 @@
 import tornado.ioloop
 import tornado.web
 
+
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("Hello, world")
+        self.write("<p>Nothing to see here, I'm just an Index. </p>"
+                   "Send some data to the following endpoint for loan "
+                   "evaluation: <br />"
+                   "<i>POST /loan</i>")
+
+
+class LoanHandler(tornado.web.RequestHandler):
+    def post(self):
+        self.write("All we need is loan <3")
+
 
 def make_app():
     return tornado.web.Application([
         (r"/", MainHandler),
+        (r"/loan", LoanHandler),
     ])
 
 if __name__ == "__main__":
